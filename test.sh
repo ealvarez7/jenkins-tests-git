e242dab237e6c9ac579fdd7ac39e454c70e4a313
root <edgar.alvarez.07@gmail.com>
2019-05-24 01:43:58 +0200

    new_update

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'Esta máquina tiene un systema operativo:' && cat /etc/issue 
-echo 'Y su hostname es:' && cat /etc/hostnamecho 'A continuación se muestra la configuración de red:'e
+echo 'Y su hostname es:' && cat /etc/hostname
 echo 'A continuación se muestra la configuración de red:'
 ifconfig
